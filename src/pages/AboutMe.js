481a5d78f088956f54b2b20c93c013bd66714e47
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-07 15:04:33 +0300

    About me sketup

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,28 @@
+import "./css/aboutme.css"
 const AboutMe = () =>
 {
 
     return(
         <div className="aboutme">
-            <h1>About Me</h1>
+            <div className="hero">
+                <h1>Kosgei Victor</h1>
+                <h3>Software Contractor </h3>
+            </div>
+            <div className="contents">
+                <div className="cover">
+                <p>
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+                </p>
+                </div>
+                <div className="contactme">
+                    <ul>
+                        <li>LinkedIn</li>
+                        <li>Email</li>
+                        <li>Twitter</li>
+                        <li>Medium</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
