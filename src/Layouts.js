378c0fadeb12e509da317f4096a80129810e0595
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-07 14:13:14 +0300

    Layouts

--- src/Layouts.js
+++ src/Layouts.js
@@ -1,17 +1,16 @@
 import {Outlet} from "react-router-dom"
 import Footer from "./pages/components/Footer"
 import Navbar from "./pages/components/Navbar"
-
 function Layout({props})
 {
     return(
-        <>
+        <div className="container">
             <Navbar/>
             <main>
                 <Outlet/>
             </main>
             <Footer/>
-        </>
+        </div>
     )
 }
 export default Layout;
