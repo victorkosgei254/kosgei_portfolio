6bdf8b90cb6eda309c5e6d5655b886dc1353d030
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-07 15:29:23 +0300

    More styling

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -10,11 +10,13 @@ const AboutMe = () =>
             </div>
             <div className="contents">
                 <div className="cover">
+                    <h1>About Me</h1>
                 <p>
                     There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
                 </p>
                 </div>
                 <div className="contactme">
+                    <h1>Contacts</h1>
                     <ul>
                         <li>LinkedIn</li>
                         <li>Email</li>
