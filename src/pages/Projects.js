515b0475fff4a4afd46d7706de43d2d3be0fc23e
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-09 10:33:01 +0300

    Projects Page Styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 import "./css/projects.css"
 import ProjectView from "./components/Project"
-import TechInfo from "./components/TechInfo";
+import TechInfo from "./components/TechInfo"
 function Projects()
 {
     return(
@@ -9,12 +9,19 @@ function Projects()
             <h1>Projects</h1>
                 <p>Websites, Web, Mobile, Desktop applications, Libraries and Frameworks</p>
             </div>
-
+            <div className="projects__contents">
             <div className="project-items">
-                <ProjectView />
-                <TechInfo />
+                
+                <ProjectView order={1}/>
+                <ProjectView order={2}/>
+                <ProjectView order={1}/>
+                <ProjectView order={2}/>
+                <ProjectView order={1}/>
+                <ProjectView order={2}/>
+                <ProjectView order={1}/>
             </div>
-
+                <TechInfo/>
+            </div>
         </div>
     )
 }
--- src/pages/components/Project.js
+++ src/pages/components/Project.js
@@ -1,37 +1,31 @@
-function ProjectView({props})
+function ProjectView({order})
 {
+    return(order % 2 === 0 ?
+        <>
+            <div className="project-view__description">
+                <p>Project Description</p>
+                <p>Another one and another</p>
+            </div>
+            <div className="project-view__line">
+                <div className="project-view__line project-view__dot"/>
+            </div>
+            <div className="project-view__image">
+                <h2>Project View Image</h2>
+            </div>
+        </>
+        :
+        <>
+            <div className="project-view__image">
+                <h2>Project View Image</h2>
+            </div>
+            <div className="project-view__line">
+                <div className="project-view__line project-view__dot"/>
+            </div>
+            <div className="project-view__description">
+                <p>Project Description</p>
+            </div>
 
-    return(
-        <div className="project-view">
-            <div className="title">
-                <h3>Swaver</h3>
-            </div>
-            <div className="description">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/1/10/Websocket_connection.png"/>
-
-                <p>An RFC6544 compliant web socket server used by web services in a distributed architecture system
-                    .Database to hold Active IP addresses, implements Publisher Subscriber. Push notification
-                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                </p>
-            </div>
-            <div className="info">
-                <div className="techstack">
-                    <span>
-                        <b>C</b>
-                        <b>Web</b>
-                    </span>
-                </div>
-                <div className="cta">
-                    <span>
-                        <a href="#">Github</a>
-                        <a href="#">Swiver</a>
-                    </span>
-                </div>
-                <div className="license">
-                    <p>MIT</p>
-                </div>
-            </div>
-        </div>
+        </>
     )
 }
 
